0fe46a210c12b60ef3b5473ebb82a225aaeccfae
c01d-br0th3r <lchyung1998@gmail.com>
2021-04-20 10:34:58 +0900

    chore: init project

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+import Explore from './src/screens/explore';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Hello World</Text>
+      <Explore />
+    </View>
+  );
+};
+
+export default App;

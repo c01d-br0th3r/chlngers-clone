a5a7da831813e0b9b097a617c305e4d0c7694417
c01d-br0th3r <lchyung1998@gmail.com>
2021-04-21 14:03:35 +0900

    feat(screens): add default screen components

---
+++ src/screens/GenerateScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text, SafeAreaView } from 'react-native';
+
+const GenerateScreen = () => {
+  return (
+    <SafeAreaView>
+      <Text>Generate</Text>
+    </SafeAreaView>
+  );
+};
+
+export default GenerateScreen;
